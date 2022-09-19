f28f0a463d5891837dcc0a58a11a0e5cd599214d
Kim Songhyun <yeonmu4121@gmail.com>
2022-09-19 19:25:44 +0900

    Implement countSqSumElement

--- src/calculate.spec.ts
+++ src/calculate.spec.ts
@@ -1,12 +1,27 @@
 import { describe, it, expect } from 'vitest';
-import { getNumList } from './calculate';
+import { countSqSumElement, getNumList } from './calculate';
 
-describe('calculate', () => {
-  it('getNumList', () => {
+describe('getNumList', () => {
+  it('returns square sum list', () => {
     expect(getNumList({ N: 400, a: 3, b: 1, allowNegative: false }))
       .toEqual([1, 16, 49, 100, 169, 256, 361]);
 
     expect(getNumList({ N: 400, a: 3, b: 1, allowNegative: true }))
       .toEqual([1, 4, 16, 25, 49, 64, 100, 121, 169, 196, 256, 289, 361, 400]);
+  });
+});
+
+describe('countSqSumElement', () => {
+  it('returns square sum element count', () => {
+    expect(countSqSumElement(1, [1])).toBe(1);
+    expect(countSqSumElement(25, [1, 2, 3])).toBe(5);
+  });
+
+  it('nums cannot be empty array', async () => {
+    expect(() => countSqSumElement(1, [])).toThrow('No Nums');
+  });
+
+  it('If there is no solution, throw Impossible error', () => {
+    expect(() => countSqSumElement(5, [2])).toThrowError('Impossible');
   });
 });
--- src/calculate.ts
+++ src/calculate.ts
@@ -20,3 +20,36 @@ export const getNumList = ({ N, a, b, allowNegative }: Parameter) => {
 
   return Array.from(set).sort((a, b) => a-b);
 };
+
+export const countSqSumElement = (() => {
+  const memo = new Map<number, number>();
+  memo.set(0, 0);
+
+  const run = (N: number, nums: number[], root: boolean = true) => {
+    if (root) {
+      memo.clear();
+      memo.set(0, 0);
+    }
+
+    if (N < 0) throw new Error('Negative Number');
+    if (nums.length === 0) throw new Error('No Nums');
+
+    if (memo.has(N)) {
+      return memo.get(N) as number;
+    }
+
+    const result = nums.map(v => {
+      try {
+        return run(N - v**2, nums, false);
+      } catch (e) {
+        return -1;
+      }
+    }).filter(v => v !== -1);
+    if (result.length === 0) throw new Error('Impossible');
+
+    memo.set(N, Math.min(...result) + 1);
+    return memo.get(N) as number;
+  };
+
+  return run;
+})();
